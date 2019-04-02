6ddef7b12b071b3bf4e1b6456640548cd1c017ce
Николай <mikalaikazlou>
2019-04-02 13:29:28 +0300

    jd47

--- src/by/it/kozlov/lesson04/TaskC1.java
+++ src/by/it/kozlov/lesson04/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.kozlov.lesson04;
 
+import java.util.Scanner;
+
 /*
 Напишите программу которая спрашивает у пользователя:
 Какую вы хотите зарплату в $$$?
@@ -37,6 +39,139 @@ package by.it.kozlov.lesson04;
 
 */
 public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какую вы хотите зарплату в $$$?");
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int month;
+        if (a<300 || a>3000){
+            System.out.println("Мы вам перезвоним!");
+            }
+else {
+        for (month=0;month<=14;month++){
+            switch (month){
+                case 0:
+                    System.out.println("За месяц 0 начислено $0.0");
+                    break;
+                case 1:
+                    double zp1=(int)a+(((int)a*50)/100);
+                    if (zp1==666){
+                        break; }
+                    System.out.println("За январь начислено $"+zp1);
+                    break;
 
+                case 2:
+                    double zp2=(int)a+(((int)a*50)/100);
+                    if (zp2==666){
+                        break; }
+                    System.out.println("За февраль начислено $"+zp2);
+                    break;
+
+                case 3:
+                    double zp3=(int)a+(((int)a*50)/100);
+                    if (zp3==666){
+                        break; }
+                    System.out.println("За март начислено $"+zp3);
+                    break;
+
+                case 4:
+                    double zp4=(int)a+(((int)a*50)/100);
+                    if (zp4==666|| a==666){
+                        break; }
+                    System.out.println("За апрель начислено $"+zp4);
+                    break;
+
+                case 5:
+                    double zp5=(int)a+(((int)a*50)/100);
+                    if (zp5==666){
+                        break; }
+                    System.out.println("За май начислено $"+zp5);
+                    break;
+
+                case 6:
+                    double zp6=a;
+                    if (a==666){
+                        break; }
+                    System.out.println("За июнь начислено $"+zp6);
+                    break;
+
+                case 7:
+                    double zp7=a;
+                    if (a==666){
+                        break; }
+                    System.out.println("За юль начислено $"+zp7);
+                    break;
+
+                case 8:
+                    double zp8=a;
+                    if (a==666){
+                        break; }
+                    System.out.println("За август начислено $"+zp8);
+                    break;
+                case 9:
+                    double zp9=(int)a+(((int)a*50)/100);
+                    if (zp9==666|| a==666){
+                        break; }
+                    System.out.println("За сентябрь начислено $"+zp9);
+                    break;
+                case 10:
+                    double zp10=(int)a+(((int)a*50)/100);
+                    if (zp10==666|| a==666){
+                        break; }
+                    System.out.println("За октябрь начислено $"+zp10);
+                    break;
+                case 11:
+                    double zp11=(int)a+(((int)a*50)/100);
+                    if (zp11==666|| a==666){
+                        break; }
+                    System.out.println("За ноябрь начислено $"+zp11);
+                    break;
+                case 12:
+                    double zp12=(int)a+(((int)a*50)/100);
+                    if (zp12==666|| a==666){
+                        break; }
+                    System.out.println("За декабрь начислено $"+zp12);
+                    break;
+                case 13:
+                    System.out.println("За месяц 13 начислено $0.0");
+                    break;
+                case 14:
+                    System.out.println("За месяц 14 начислено $0.0");
+                    break;
+
+
+        }}}}}
+            /* if (month==0){
+                System.out.println();
+            }
+            if (month>0 && month<=5){
+                double zp1=a+((a/100)*50);
+                if (zp1==666){
+                    break;
+                }
+                System.out.println(+"\n"+"февраль начислено"+" $"+zp1+"\n"+"март начислено"+" $"+zp1+"\n"+"апрель начислено"+" $"+zp1+"\n"+"май начислено"+" $"+zp1);
+                                   }
+            if (month>5 && month<9){
+                double zp2=a;
+                if (zp2==666){
+                    break;
+                }
+                System.out.println("июнь начислено"+" $"+zp2);
+                System.out.println("июль начислено"+" $"+zp2);
+                System.out.println("август начислено"+" $"+zp2);
+            }
+            if (month>=9 && month<13){
+                double zp3=a+((a/100)*50);
+                if (zp3==666){
+                    break;
+                }
+                System.out.println("сентябрь начислено"+" $"+zp3);
+                System.out.println("октябрь начислено"+" $"+zp3);
+                System.out.println("ноябрь начислено"+" $"+zp3);
+                System.out.println("декабрь начислено"+" $"+zp3);
+                    }
+        }
+    }
 
 }
+*/
