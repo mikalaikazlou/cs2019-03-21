0a878e84ad5b0130120e0c99c39bd3b49c03ef5d
Николай <mikalaikazlou>
2019-04-02 11:28:53 +0300

    jd46

--- src/by/it/kozlov/lesson04/TaskB3.java
+++ src/by/it/kozlov/lesson04/TaskB3.java
@@ -25,7 +25,20 @@ package by.it.kozlov.lesson04;
  */
 
 
+import java.util.Scanner;
 
 public class TaskB3 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+        int i;
+        for (i = 1; sum <= a; i++) {
+            sum += i;
+            if(sum> a) { break;
+            }
+            else
+            System.out.print(sum + ",");
+        }
+    }
 }
