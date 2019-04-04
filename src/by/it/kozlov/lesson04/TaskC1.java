1fd833b333516f8aaf02ca45cfc6e73681ea4fdd
Николай <mikalaikazlou>
2019-04-04 10:44:40 +0300

    jd47

--- src/by/it/kozlov/lesson04/TaskC1.java
+++ src/by/it/kozlov/lesson04/TaskC1.java
@@ -51,92 +51,89 @@ else {
         for (month=0;month<=14;month++){
             switch (month){
                 case 0:
-                    System.out.println("За месяц 0 начислено $0.0");
+                    System.out.print("За месяц 0 начислено $0.0"+"\n");
                     break;
                 case 1:
                     double zp1=(int)a+(((int)a*50)/100);
-                    if (zp1==666){
-                        break; }
-                    System.out.println("За январь начислено $"+zp1);
+
+                    System.out.print("За январь начислено $"+zp1+"\n");
                     break;
 
                 case 2:
                     double zp2=(int)a+(((int)a*50)/100);
                     if (zp2==666){
                         break; }
-                    System.out.println("За февраль начислено $"+zp2);
+                    System.out.print("За февраль начислено $"+zp2+"\n");
                     break;
 
                 case 3:
                     double zp3=(int)a+(((int)a*50)/100);
                     if (zp3==666){
                         break; }
-                    System.out.println("За март начислено $"+zp3);
+                    System.out.print("За март начислено $"+zp3+"\n");
                     break;
 
                 case 4:
                     double zp4=(int)a+(((int)a*50)/100);
-                    if (zp4==666|| a==666){
+                    if (zp4==666){
                         break; }
-                    System.out.println("За апрель начислено $"+zp4);
+                    System.out.print("За апрель начислено $"+zp4+"\n");
                     break;
 
                 case 5:
                     double zp5=(int)a+(((int)a*50)/100);
                     if (zp5==666){
                         break; }
-                    System.out.println("За май начислено $"+zp5);
+                    System.out.print("За май начислено $"+zp5+"\n");
                     break;
 
                 case 6:
-                    double zp6=a;
-                    if (a==666){
+                    if (a!=666){
+                        System.out.print("За июнь начислено $"+ (double) (int)a +"\n");
                         break; }
-                    System.out.println("За июнь начислено $"+zp6);
+                    else {System.out.print("За июнь начислено $"+ (double) (int)a +"\n");}
                     break;
 
                 case 7:
-                    double zp7=a;
                     if (a==666){
                         break; }
-                    System.out.println("За юль начислено $"+zp7);
+                    System.out.print("За июль начислено $"+ (double) a +"\n");
                     break;
 
                 case 8:
-                    double zp8=a;
                     if (a==666){
                         break; }
-                    System.out.println("За август начислено $"+zp8);
+                    System.out.print("За август начислено $"+ (double) a +"\n");
                     break;
                 case 9:
                     double zp9=(int)a+(((int)a*50)/100);
-                    if (zp9==666|| a==666){
+                    if (zp9==666){
                         break; }
-                    System.out.println("За сентябрь начислено $"+zp9);
+                    System.out.print("За сентябрь начислено $"+zp9+"\n");
                     break;
                 case 10:
                     double zp10=(int)a+(((int)a*50)/100);
-                    if (zp10==666|| a==666){
+                    if (zp10==666){
                         break; }
-                    System.out.println("За октябрь начислено $"+zp10);
+                    System.out.print("За октябрь начислено $"+zp10+"\n");
                     break;
                 case 11:
                     double zp11=(int)a+(((int)a*50)/100);
-                    if (zp11==666|| a==666){
+                    if (zp11==666){
                         break; }
-                    System.out.println("За ноябрь начислено $"+zp11);
+                    System.out.print("За ноябрь начислено $"+zp11+"\n");
                     break;
                 case 12:
                     double zp12=(int)a+(((int)a*50)/100);
-                    if (zp12==666|| a==666){
+                    if (zp12==666){
                         break; }
-                    System.out.println("За декабрь начислено $"+zp12);
+                    System.out.print("За декабрь начислено $"+zp12+"\n");
                     break;
                 case 13:
-                    System.out.println("За месяц 13 начислено $0.0");
+                    System.out.print("За месяц 13 начислено $0.0"+"\n");
                     break;
                 case 14:
-                    System.out.println("За месяц 14 начислено $0.0");
+                    System.out.print("За месяц 14 начислено $0.0"+"\n");
                     break;
 
 
